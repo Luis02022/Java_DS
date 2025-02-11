edaa6727da271a1f17f95e3d8f7339ba6994ad01
Luis02022 <160199124+Luis02022@users.noreply.github.com>
2025-02-11 11:18:44 -0300

    Add files via upload

--- For_Repeticao/src/main/java/com/mycompany/for_repeticao/Array_Exerc.java
+++ For_Repeticao/src/main/java/com/mycompany/for_repeticao/Array_Exerc.java
@@ -3,11 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.for_repeticao;
-
+import java.util.ArrayList;
+import java.util.Scanner;
 /**
  *
  * @author aluno.den
  */
 public class Array_Exerc {
-    
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        ArrayList<Double> notas = new ArrayList<>();
+        String resposta;
+        
+        
+        do{
+            System.out.println("Digite uma nota:");
+            notas.add(ler.nextDouble());
+                    
+            System.out.println("Deseja inserir mais uma nota?");
+            System.out.println("\nPressione a tecla N para sair.");
+            resposta = ler.next();
+                    
+        }while(!resposta.equalsIgnoreCase("n"));
+       
+        System.out.println("\n||EXIBINDO DADOS||");
+        for(double nota: notas){
+            System.out.println("Nota: "+ nota);
+        }
+        
+        
+    }
 }
